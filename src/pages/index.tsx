23303dace86df70bbca5d3e169a328aa7e3abe4a
ebosspc <software2070@protonmail.com>
2025-06-15 02:09:14 -0400

    got initial logo up and running

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,8 +33,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Welcome to ${siteConfig.title}`}
+      description="IronShield Security Documentation">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
